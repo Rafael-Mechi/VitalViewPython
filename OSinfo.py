9ae0bba708fa8b87370d2748aefb3927965e7566
pdrzewski <pedro.martins@sptech.school>
2025-08-26 12:33:46 -0300

    Add files via upload
    OS reading script

---
+++ OSinfo.py
@@ -0,0 +1,33 @@
+import platform
+import os
+import pandas as pd
+
+system_info = platform.uname()
+
+CSV_PATH = "dados_OS.csv"
+arquivo_existe = os.path.exists(CSV_PATH)
+
+marca = """
+__     ___ _        _  __     ___               
+\ \   / (_) |_ __ _| | \ \   / (_) _____      __
+ \ \ / /| | __/ _` | |  \ \ / /| |/ _ \ \ /\ / /
+  \ V / | | || (_| | |   \ V / | |  __/\ V  V / 
+   \_/  |_|\__\__,_|_|    \_/  |_|\___| \_/\_/  
+"""
+
+print(marca)
+
+print(f"OS: {system_info.system}")
+print(f"User & Device: {system_info.node}")
+print(f"Version: {system_info.version}")
+print(f"Machine: {system_info.machine}")
+
+dados = pd.DataFrame([{
+        "OS": system_info.system,
+        "User & Device": system_info.node,
+        "Version": system_info.version,
+        "Machine": system_info.machine
+    }])
+
+dados.to_csv(CSV_PATH, mode='a', header=not arquivo_existe, index=False, sep=';')
+arquivo_existe = True
