9cd042f089ddeca31ed1021edcf300728290da56
pdrzewski <pedro.martins@sptech.school>
2025-08-26 13:37:00 -0300

    Process data reading

---
+++ process.py
@@ -0,0 +1,34 @@
+import psutil
+import pandas as pd
+import os
+
+CSV_PATH = "process_data.csv"
+arquivo_existe = os.path.exists(CSV_PATH)
+
+wtrmark = """
+__     ___ _        _  __     ___               
+\ \   / (_) |_ __ _| | \ \   / (_) _____      __
+ \ \ / /| | __/ _` | |  \ \ / /| |/ _ \ \ /\ / /
+  \ V / | | || (_| | |   \ V / | |  __/\ V  V / 
+   \_/  |_|\__\__,_|_|    \_/  |_|\___| \_/\_/  
+"""
+
+print(wtrmark)
+
+for proc in psutil.process_iter(['name', 'username', 'pid', 'memory_percent']):
+    info = proc.info
+    username = info.get('username')  
+    name = info.get('name')         
+    pid = info.get('pid')
+    memoryPercent = info.get('memory_percent') 
+    print(f"Usuario: {username}, Processo: {name}, pid: {pid}, RAM usada: {memoryPercent:.4f}")
+
+    dados = pd.DataFrame([{
+        "username": username,
+        "name": name,
+        "pid": pid,
+        "memory_percent": memoryPercent
+    }])
+
+    dados.to_csv(CSV_PATH, mode='a', header=not arquivo_existe, index=False, sep=';')
+    arquivo_existe = True
