6b374c8814156786cc824e86bab98ccb4773b62e
lucas <lucas.rodrigues127@sptech.school>
2025-09-29 11:10:02 -0300

    script de captura geral para analise

---
+++ scripts/capturaGeral.py
@@ -0,0 +1,99 @@
+import psutil
+import pandas as pd
+import os
+import time
+import socket
+from datetime import datetime
+from psutil._common import bytes2human
+
+# Obs:
+# O valor de "Uso de CPU" retornado por psutil.cpu_percent() representa o uso total de todos os núcleos da CPU.
+# Ou seja, em máquinas com múltiplos núcleos, o valor pode ultrapassar 100%.
+# Exemplo: em um sistema com 8 núcleos, o valor máximo possível é 800%.
+
+DATA_PATH = "../data"
+CSV_PATH = "../data/process_data.csv"
+
+os.makedirs(DATA_PATH, exist_ok=True) # Se nao existir, ele cria 
+
+
+wtrmark = """
+__     ___ _        _  __     ___               
+\ \   / (_) |_ __ _| | \ \   / (_) _____      __
+ \ \ / /| | __/ _` | |  \ \ / /| |/ _ \ \ /\ / /
+  \ V / | | || (_| | |   \ V / | |  __/\ V  V / 
+   \_/  |_|\__\__,_|_|    \_/  |_|\___| \_/\_/  
+"""
+
+print(wtrmark)
+
+try:
+    while True:
+
+        linhas = []
+
+        for proc in psutil.process_iter(['name', 'username', 'pid', 'memory_percent', 'num_threads', 'create_time','status']):
+            try:
+                info = proc.info
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                create_time_human = datetime.fromtimestamp(info.get('create_time')).strftime("%Y-%m-%d %H:%M:%S") \
+                                    if info.get('create_time') else ""
+                memory_human = bytes2human(proc.memory_info().rss)
+                cpu_usage = proc.cpu_percent(interval=0.1)
+                hostname = socket.gethostname()
+
+                cpu_percent = psutil.cpu_percent(interval=0.1, percpu=False)
+
+                mem = psutil.virtual_memory()
+                mem_percent = mem.percent
+                mem_avl = bytes2human(mem.available)
+    
+                disk = psutil.disk_usage('/')
+                disk_percent = disk.percent
+                disk_avl = bytes2human(disk.free)
+
+                linhas.append({
+                    "Nome da Máquina": hostname,
+                    "Data da Coleta": timestamp,
+                    #"Usuario": info.get('username'),
+                    "Processo": info.get('name'),
+                    "ID": info.get('pid'),
+                    'Uso de CPU': [cpu_percent],
+                    'Uso de RAM': [mem_percent],
+                    'Memória Disponível': [mem_avl],
+                    'Uso de Disco': [disk_percent],
+                    'Disponibilidade do Disco': [disk_avl],
+                    "Uso de Ram": memory_human,
+                    "Uso de CPU":  round(cpu_usage,2),
+                    "Uso de Threads": info.get('num_threads'),
+                    "Quando foi iniciado": create_time_human,
+                    "Status": info.get('status')
+                })
+                
+                print(f"Timestamp: {timestamp},Usuario: {info.get('username')}, Processo: {info.get('name')}, pid: {info.get('pid')}, RAM usada: {memory_human}, CPU usada no proceso: { round(cpu_usage,2)},Numero de threads: {info.get('num_threads')}, Horario que processo iniciou: {create_time_human}, Status: {info.get('status')} ")
+
+                dados = pd.DataFrame(linhas)
+
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
+        
+            #if os.path.exists(DATA_PATH):
+            #   dados.to_csv(CSV_PATH, mode="a", sep=';', encoding='utf-8', index=False, header=False)
+            #else:
+            #   os.mkdir(DATA_PATH)
+            #    dados.to_csv(CSV_PATH, mode="a", sep=';', encoding='utf-8', index=False, header=False)
+
+            if os.path.exists(CSV_PATH):
+            # próximas vezes → só adiciona sem header
+                dados.to_csv(CSV_PATH, mode="a", sep=";", encoding="utf-8", index=False, header=False)
+            else:
+            # primeira vez → cria com header
+                dados.to_csv(CSV_PATH, mode="w", sep=";", encoding="utf-8", index=False, header=True)
+
+            time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\nMonitoramento finalizado.")
+
+    
+
